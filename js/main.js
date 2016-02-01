e089cda329465247302ce11855295871e1f4e2e3
Rana Faiz Ahmed <iamfaizahmed123@gmail.com>
2016-02-02 00:31:59 +0500

    Make the container width smaller

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@ $(function () {
 	$(window).on('scroll', function () {
 		if ( $(this).scrollTop() !== 0 ) {
 			$('.navbar').addClass('navbar-scrolled');
-			console.log('not zero');
 		} else {
-			console.log('zero');
 			$('.navbar').removeClass('navbar-scrolled');
 		}
 	});
