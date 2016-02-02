4d66f8866f57a4394f1393b79a0c87fbe48a1f33
Rana Faiz Ahmed <iamfaizahmed123@gmail.com>
2016-02-02 17:22:25 +0500

    Fix some more stuff

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
 $(function () {
+	// Scrolling navbar effect.
 	$(window).on('scroll', function () {
 		if ( $(this).scrollTop() > 10 ) {
 			$('.navbar').addClass('navbar-scrolled');
 		} else {
 			$('.navbar').removeClass('navbar-scrolled');
 		}
+	});
+
+	// Login and Register in on modal
+	var $loginRegisterModal = $('#login-register-modal');
+	$('a[data-open="register"]').on('click', function (event) {
+		event.preventDefault();
+
+		$loginRegisterModal.addClass('show-register');
+		$loginRegisterModal.removeClass('show-login');
+	});
+
+	$('a[data-open="login"]').on('click', function (event) {
+		event.preventDefault();
+
+		$loginRegisterModal.addClass('show-login');
+		$loginRegisterModal.removeClass('show-register');
+	});
+
+	$('.btn-buy-now').on('click', function (event) {
+		event.preventDefault();
+
+		$('#login-register-modal').modal('show');
 	});
 });
