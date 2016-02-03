41e49cfc9c7f25c62db5a9b864775de8edc782a0
Rana Faiz Ahmed <iamfaizahmed123@gmail.com>
2016-02-03 12:23:03 +0500

    Item count controls

--- js/main.js
+++ js/main.js
@@ -29,10 +29,22 @@ $(function () {
 
 		$('#login-register-modal').modal('show');
 	});
-});
 
-$('#item-count').on('change', function (event) {
-	if (parseInt($(this).val()) < 0) {
-		$(this).val(0);
-	}
+
+	$('.spinner .btn:first-of-type').on('click', function() {
+		$('.spinner input').val( parseInt($('.spinner input').val()) + 1);
+	});
+	$('.spinner .btn:last-of-type').on('click', function() {
+		var currentValue = parseInt($('.spinner input').val());
+		if ( (currentValue - 1) >= 1 ) {
+			$('.spinner input').val( currentValue - 1);
+		}
+	});
+
+	$('#item-count').on('change', function (event) {
+		if (parseInt($(this).val()) < 0) {
+			$(this).val(0);
+		}
+	});
+
 });
