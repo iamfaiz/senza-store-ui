57898ca7eed8ee14329c046c54413509c70b9c6c
Rana Faiz Ahmed <iamfaizahmed123@gmail.com>
2016-02-03 15:35:47 +0500

    Cart Page

--- js/main.js
+++ js/main.js
@@ -32,19 +32,17 @@ $(function () {
 
 
 	$('.spinner .btn:first-of-type').on('click', function() {
-		$('.spinner input').val( parseInt($('.spinner input').val()) + 1);
+		var $input = $(this).parent().siblings('input');
+
+		$input.val( parseInt($input.val()) + 1);
 	});
+
 	$('.spinner .btn:last-of-type').on('click', function() {
-		var currentValue = parseInt($('.spinner input').val());
+		var $input = $(this).parent().siblings('input');
+		var currentValue = parseInt($input.val());
+
 		if ( (currentValue - 1) >= 1 ) {
-			$('.spinner input').val( currentValue - 1);
+			$input.val( currentValue - 1);
 		}
 	});
-
-	$('#item-count').on('change', function (event) {
-		if (parseInt($(this).val()) < 0) {
-			$(this).val(0);
-		}
-	});
-
 });
